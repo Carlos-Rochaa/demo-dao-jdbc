c085040efae6d08453c42e28f740c1fad810ea48
Carlos Rocha <carlosrocha1896@gmail.com>
2023-12-29 18:04:22 -0300

    SellerDaoJDBC

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.util.Date;
 
@@ -8,6 +11,8 @@ public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
